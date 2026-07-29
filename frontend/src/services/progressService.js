4c655658789a6215e8b3a768e51928941f3dbbbc
hiii129 <24100227@st.phenikaa-uni.edu.vn>
2026-07-29 11:15:01 +0700

    cập nhật chức năg quản lý tiến đọ

--- frontend/src/services/progressService.js
+++ frontend/src/services/progressService.js
@@ -2,6 +2,15 @@ import axios from "axios";
 
 // Đổi port 5000 thành đúng Port Backend của bạn (VD: 3000, 8080)
 const API_URL = "http://localhost:3000/api/progress"; 
+function getConfig() {
+    const token = localStorage.getItem("token");
+
+    return {
+        headers: {
+            Authorization: `Bearer ${token}`
+        }
+    };
+}
 
 const progressService = {
     // 1. Sinh viên lấy lịch sử tiến độ
@@ -10,17 +19,17 @@ const progressService = {
     },
     // Giảng viên xem tiến độ theo đề tài
     getProgress: async (topicId) => {
-        const res = await axios.get(`${API_URL}/${topicId}`);
+        const res = await axios.get(`${API_URL}/${topicId}`, getConfig());
         return res.data;
     },
     // 2. Sinh viên cập nhật tiến độ
     createProgress: (data) => {
-        return axios.post(API_URL, data);
+        return axios.post(API_URL, data, getConfig());
     },
 
     // 3. Giảng viên cập nhật nhận xét
     updateComment: (id, data) => {
-        return axios.put(`${API_URL}/${id}/comment`, data);
+        return axios.put(`${API_URL}/${id}/comment`, data, getConfig());
     },
 
     // 4. Giảng viên lấy danh sách sinh viên
@@ -30,13 +39,13 @@ const progressService = {
 
     // 5. Admin lấy tất cả tiến độ
     getAllProgress: async () => {
-    const res = await axios.get(API_URL);
+    const res = await axios.get(API_URL, getConfig());;
     return res.data;
     },
 
     // 6. Admin xóa tiến độ
     deleteProgress: (id) => {
-        return axios.delete(`${API_URL}/${id}`);
+        return axios.delete(`${API_URL}/${id}`, getConfig());
     }
 };
 
